55d53ea341f79acd10c84b400299832bc2cbfbcf
李少凯 <lishaokai@wasu.com>
2025-12-18 16:49:29 +0800

    feat: 🎸 add .env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  runtimeConfig: {
+    // The private keys which are only available on server-side
+    databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL,
+    // Keys within public, will be also exposed to the client-side
+    public: {}
+  }
 })
